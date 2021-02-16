62c4be430baaff7ad50b7b7c2058b63fc4e27c48
JohariGourav <41551799+JohariGourav@users.noreply.github.com>
2021-02-16 18:32:19 +0530

    Developed till end of video 334.Forwarding Unauthenticated Users | Add Redirect to '/' feature on '/auth' page when user login is successful | Add NO showing of orders button in NAV bar if not logged in | User see SIGNUP TO ORDER instead of ORDER NOW btn in home page if not logged in, and redirected to /auth on click

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -87,7 +87,11 @@ class BurgerBuilder extends Component {
     // };
 
     purchaseHandler = () => {
-        this.setState({ purchasing: true });
+        if(this.props.isAuthenticated) {
+            this.setState({ purchasing: true });
+        } else {
+            this.props.history.push('/auth');
+        }
     }
 
     purchaseCancelHandler = () => {
@@ -148,6 +152,7 @@ class BurgerBuilder extends Component {
                         disabled={disabledInfo}
                         purchaseable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler}
+                        isAuth={this.props.isAuthenticated}
                         price={this.props.price}
                     />
                 </Aux>
@@ -180,6 +185,7 @@ const mapStateToProps = state => {
         ings: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
         error: state.burgerBuilder.error,
+        isAuthenticated: state.auth.token !== null,
     }
 }
 const mapDispatchToProps = dispatch => {

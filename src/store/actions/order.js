06a35b09f25eb2f659b23a46d54d912fddd1f775
JohariGourav <41551799+JohariGourav@users.noreply.github.com>
2021-02-19 18:14:50 +0530

    Developed till 340. Displaying User Specific Orders | Sent 'userId' value to backend while ordering | Change '/orders' axios request to only fetch user specific orders

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -65,10 +65,11 @@ export const fetchOrdersStart = () => {
 
 // async action when user clicks ORDERS BUTTON
 
-export const fetchOrders = (token) => {
+export const fetchOrders = (token, userId) => {
     return (dispatch, getState) => {
         dispatch(fetchOrdersStart());
-        axios.get('/orders.json?auth=' + token)
+        const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
+        axios.get('/orders.json' + queryParams)
             .then(res => {
                 const fetchedOrders = [];
                 for (let key in res.data) {
